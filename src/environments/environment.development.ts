bc18f4734e8c47e1350dffadaf7e43586c368cbd
FR13ND-ly <motricala44@gmail.com>
2025-05-23 20:57:59 +0300

    add backend url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     dev: false,
-    apiUrl: 'http://127.0.0.1:5000/',
+    // apiUrl: 'http://127.0.0.1:5000/',
+    apiUrl: 'https://researches-backend.vercel.app/',
   };
   
